d6ec528a854d180da994cdc593064495543ccc8a
Burak Karahan <119459005+BurakKarahan8@users.noreply.github.com>
2025-01-05 18:07:30 +0300

    değişiklik
    silindi

--- vtys_front/my-react-app/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import energyReducer from './slices/energySlice';
-
-export const store = configureStore({
-  reducer: {
-    energy: energyReducer,
-  },
-}); 
